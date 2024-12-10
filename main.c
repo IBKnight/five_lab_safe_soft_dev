30008e4fc8c3e7880db7c3bf9e514d1e9e715a78
bondarevis <bondarevis@bondarev.myguest.virtualbox.org>
2024-12-10 21:06:07 +0300

    changed for debug

--- main.c
+++ main.c
@@ -12,25 +12,20 @@ int main()
   char buf[40];
 
   // fgets(buf,45,stdin);
-  gets(buf);
+  // gets(buf);
   // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\xef\xbe\xad\xde
-  // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAﾭ�
+  // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAﾭS�
   // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAï¾­Þ
   // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA����
 
-  // printf("Before %p\n", (void*)&check);
-
-  // memset(buf, 'A', 40);
+  memset(buf, 'A', 40);
 
 
-  // buf[44] = '\xef';
-  // buf[45] = '\xbe';
-  // buf[46] = '\xad';
-  // buf[47] = '\xde';
-  // buf[48] = '\0';
-
-  printf("After %p\n", (void*)&check);
-  
+  buf[44] = '\xef';
+  buf[45] = '\xbe';
+  buf[46] = '\xad';
+  buf[47] = '\xde';
+  buf[48] = '\0';
 
   printf("\n[buf]: %s\n", buf);
   printf("[check] %p\n", check);
@@ -42,39 +37,8 @@ int main()
    {
      printf("Yeah dude! You win!\nOpening your shell...\n");
      setreuid(geteuid(), geteuid());
-     system("/bin/bash -i");
+     system("gnome-terminal");
      printf("Shell closed! Bye.\n");
    }
    return 0;
 }
-
-
-// #include <stdio.h>
-// #include <string.h>
-
-// int main(void)
-// {
-//     char buff[15];
-//     int pass = 0;
-
-//     printf("\n Enter the password : \n");
-//     gets(buff);
-
-//     if(strcmp(buff, "thegeekstuff"))
-//     {
-//         printf ("\n Wrong Password \n");
-//     }
-//     else
-//     {
-//         printf ("\n Correct Password \n");
-//         pass = 1;
-//     }
-
-//     if(pass)
-//     {
-//        /* Now Give root or admin rights to user*/
-//         printf ("\n Root privileges given to the user \n");
-//     }
-
-//     return 0;
-// }
